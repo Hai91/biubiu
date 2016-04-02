3b881765ec5821cf39ec125523c74b54ae8ca7e5
徐云凯 <xyk35182966@yuki-2.local>
2016-04-02 10:00:24 +0800

    新建项目

---
+++ biubiu/biubiu/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  biubiu
+//
+//  Created by 徐云凯 on 16/4/2.
+//  Copyright © 2016年 Yuki. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ biubiu/biubiu/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  biubiu
+//
+//  Created by 徐云凯 on 16/4/2.
+//  Copyright © 2016年 Yuki. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
